d40c5ace7dca98e99441579b9fd14c3fdf3d10a9
clint.munden <clint.munden@notronix.com>
2019-07-17 10:58:37 -0230

    - version 1.0.0001 - added a class to illustrate the use of the JEtsy implementation.

---
+++ src/test/java/com/notronix/etsy/test/ApiTester.java
@@ -0,0 +1,37 @@
+package com.notronix.etsy.test;
+
+import com.notronix.etsy.api.EtsyAPIException;
+import com.notronix.etsy.api.authentication.Credentials;
+import com.notronix.etsy.impl.EtsyDataService;
+
+public class ApiTester
+{
+    private Object test(EtsyDataService eds, Credentials clientCreds, Credentials accessCreds)
+            throws EtsyAPIException {
+        return eds.getMethodTable(clientCreds);
+    }
+
+    /**
+     * Test Runner
+     *
+     * @param args used to supply credentials.  args must contain 4 supplied credentials as described by the code.
+     */
+    public static void main(String[] args) {
+        String consumerKey = args[0];
+        String consumerSecret = args[1];
+        String token = args[2];
+        String tokenSecret = args[3];
+
+        Credentials clientCreds = Credentials.forKeyPair(consumerKey, consumerSecret);
+        Credentials accessCreds = Credentials.forKeyPair(token, tokenSecret);
+
+        try {
+            Object result = new ApiTester().test(new EtsyDataService(), clientCreds, accessCreds);
+
+            System.out.println("done");
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
